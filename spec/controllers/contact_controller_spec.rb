c128e9e7cf746571e59b083d1b1fdbadbd480f29
kevinegstorf <kevin.egstorf@gmail.com>
2016-05-22 22:04:44 +0200

    added contact controller spec

--- spec/controllers/contact_controller_spec.rb
+++ spec/controllers/contact_controller_spec.rb
@@ -1,5 +1,10 @@
+
 require 'rails_helper'
 
-RSpec.describe ContactController, type: :controller do
-
+describe ContactController, type: :controller do
+  describe 'GET #index' do
+    before { get :index }
+    specify { expect(response).to be_success }
+    specify { expect(response).to render_template :index }
+  end
 end
